bd74cc9b308d70962eecefe1117a94d755159a33
CamiloEstradaDev2788 <devypunto@gmail.com>
2025-11-17 15:37:24 -0500

    11
    sdsd

--- src/main/java/com/bodegapp/core/database/ConexionBD.java
+++ src/main/java/com/bodegapp/core/database/ConexionBD.java
@@ -11,9 +11,9 @@ public class ConexionBD {
     private static final String USER = "CamiloEstrada";
     private static final String PASSWORD = "Ep.1028880723";
     */
-    private static final String URL = "jdbc:mysql://localhost:3307/bodegapp_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/bodegapp_db";
+    private static final String USER = "CamiloEstrada";
+    private static final String PASSWORD = "Ep.1028880723";
     
     private static Connection conn = null;
     
